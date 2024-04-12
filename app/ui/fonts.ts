5f792477a7f40f7ad27a4ca040a56f365038d309
Jade van Dorsten <jade@van-dorsten.net>
2024-04-12 15:32:44 -0500

    Import fonts `Inter`, `Lora`

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Lora } from 'next/font/google';
+ 
+export const inter = Inter({
+   subsets: ['latin'],
+});
+export const lora = Lora({
+   subsets: ['latin'],
+   style: ['normal', 'italic'],
+});
