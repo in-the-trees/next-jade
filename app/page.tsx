b4c2817f5701464ef48c74314dc93ff38a0ec622
Jade van Dorsten <jade@van-dorsten.net>
2024-04-12 16:04:21 -0500

    Responsive home top padding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
    return (
       <main>
          <div className="flex justify-center items-center">
-            <Jade className="mt-8 w-full max-w-2xl" />
+            <Jade
+               className="
+                  pt-2
+                  md:pt-4
+                  lg:pt-8
+                  w-full max-w-2xl
+               "
+            />
          </div>
       </main>
    );
