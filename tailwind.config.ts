03c4a01a577f8bab43826ba875a18cf31b073444
Jade van Dorsten <jade@van-dorsten.net>
2024-04-12 14:38:12 -0500

    Extend Tailwind fontWeight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,27 @@ const config: Config = {
         },
     },
     plugins: [],
+   theme: {
+      extend: {
+         backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+               "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+         },
+         fontWeight: {
+            'thin-mid': '150',
+            'extralight-mid': '250',
+            'light-mid': '350',
+            'normal-mid': '450',
+            'medium-mid': '550',
+            'semibold-mid': '650',
+            'bold-mid': '750',
+            'extrabold-mid': '850',
+         },
+         margin: {
+            '1.75': '0.4375rem',
+         },
+      },
+   },
 };
 export default config;
