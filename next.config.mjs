16ea5660aa256d36e3a6f83541f41664eca1c187
Jade van Dorsten <jade@van-dorsten.net>
2024-04-13 12:28:55 -0500

    Support Cloudflare Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 import withMDX from "@next/mdx";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+if (process.env.NODE_ENV === "development") {
+   await setupDevPlatform({
+      persist: true,
+   });
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
