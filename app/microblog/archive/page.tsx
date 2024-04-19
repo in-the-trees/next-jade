b800b0cba5fc069e2fcb0062fbc2ba3b497e65e2
Jade van Dorsten <jade@van-dorsten.net>
2024-04-18 20:44:31 -0500

    Implement microblog archive page

--- app/microblog/archive/page.tsx
+++ app/microblog/archive/page.tsx
@@ -1,31 +1,21 @@
 import MicroblogFeed from "@/app/_components/microblog/MicroblogFeed";
 import { lora } from "@/app/_fonts/fonts";
-import {
-   ArchiveBoxIcon,
-   GlobeAltIcon,
-   RssIcon,
-} from "@heroicons/react/24/outline";
+import { GlobeAltIcon, RssIcon } from "@heroicons/react/24/outline";
 
 export default function Blog() {
    return (
       <main id="microblog">
          <h1 className={`${lora.className} mb-3.5 text-[1.5em] font-normal-mid`}>
-            Microblog
+            Microblog archive
          </h1>
          <h2>
             Welcome to my microblog where I publish short, tweet-like posts. I
             don't take my microblog too seriously, nor should you. By default,
-            only posts within the last day are visible.
+            only posts within the last day are visible, however, here—in the
+            archive—all public posts are displayed.
          </h2>
 
          <div className="my-4 flex flex-wrap items-center gap-2">
-            <a
-               href="/microblog/archive"
-               className="flex w-max items-center justify-between gap-2 rounded-lg border px-2 py-1 text-sm shadow-sm transition-transform ease-out hover:scale-103"
-            >
-               <ArchiveBoxIcon className="h-4 w-4" />
-               Archive
-            </a>
             <a
                href=""
                className="flex w-max items-center justify-between gap-2 rounded-lg border px-2 py-1 text-sm shadow-sm transition-transform ease-out hover:scale-103"
@@ -44,8 +34,7 @@ export default function Blog() {
          </div>
 
          <MicroblogFeed
-            feedUrl="https://jade.van-dorsten.net/api/recent.json"
-            cutoffInHours={24}
+            feedUrl="https://jade.van-dorsten.net/api/all.json"
             className="mt-9"
          />
       </main>
