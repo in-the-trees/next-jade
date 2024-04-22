57989c6e59b80004d7454ad15bba9cc476d4f4e1
Jade van Dorsten <jade@van-dorsten.net>
2024-04-21 22:24:25 -0500

    Create a custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Jade from "@/app/_components/Jade";
+import Breadcrumb from "@/app/_components/breadcrumb";
+import { lora } from "@/app/_fonts/fonts";
+
+export default function NotFound() {
+   return (
+      <div className="flex w-full justify-center">
+         <div className="flex w-full max-w-[70rem] gap-2 md:h-full">
+            <aside className="sticky top-0 hidden w-1/3 self-start overflow-y-scroll md:block md:pt-[44px] lg:pt-[60px]">
+               <Jade className="max-h-screen overflow-auto px-4" />
+            </aside>
+            <div className="w-full md:w-2/3">
+               <div>
+                  <header className="sticky top-0 z-50 px-4">
+                     <div className="bg-white pt-7 md:pt-[44px] lg:pt-[60px]">
+                        <Breadcrumb
+                           items={[
+                              { type: "link", text: "Jade", href: "/" },
+                              { type: "separator" },
+                              { type: "text", text: "404" },
+                           ]}
+                        />
+                     </div>
+                     <div className="bg-gradient-to-b from-white pb-5"></div>
+                  </header>
+                  <main className="px-4">
+                     <h1
+                        className={`${lora.className} mb-3.5 text-[1.5em] font-normal-mid`}
+                     >
+                        404
+                     </h1>
+                     <h2>Page not found</h2>
+                  </main>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
