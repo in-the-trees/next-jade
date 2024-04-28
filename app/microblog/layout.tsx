9d42142b83fdbd147fdf2ef26af43abb801159bf
Jade van Dorsten <jade@inthetrees.me>
2024-04-27 21:53:18 -0500

    Swap layout ordering and use row-reverse

--- app/microblog/layout.tsx
+++ app/microblog/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
    return (
       <div className="flex w-full justify-center">
-         <div className="flex w-full max-w-[70rem] gap-2 md:h-full">
+         <div className="flex w-full max-w-[70rem] flex-row-reverse gap-2 md:h-full">
+            <div className="w-full md:w-2/3">{children}</div>
             <aside className="sticky top-0 hidden w-1/3 self-start overflow-y-scroll md:block md:pt-[44px] lg:pt-[60px]">
                <Jade className="max-h-screen overflow-auto px-4" />
             </aside>
-            <div className="w-full md:w-2/3">{children}</div>
          </div>
       </div>
    );
