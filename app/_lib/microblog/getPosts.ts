c99534b99e11e478d3592cf97f267f515ec9d866
Jade van Dorsten <jade@inthetrees.me>
2024-05-14 00:37:29 -0500

    Revalidate feed in microblog post getter

--- app/_lib/microblog/getPosts.ts
+++ app/_lib/microblog/getPosts.ts
@@ -1,7 +1,10 @@
-export default async function getPosts(feed: URL) {
+import { revalidatePath } from "next/cache";
+
+export default async function getPosts(feed: string) {
+   revalidatePath(feed.toString());
    const res = await fetch(feed);
    if (!res.ok) {
-      throw new Error('Failed to fetch feed');
+      throw new Error("Failed to fetch feed");
    } else {
       const json = await res.json();
       return json.items;
