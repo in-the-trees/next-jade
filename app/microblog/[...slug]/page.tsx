0fbf4fbc22ebec5d6a13a6c8300e0f2b2de4b3a9
Jade van Dorsten <jade@inthetrees.me>
2024-05-14 18:33:37 -0500

    Initial dynamic microblog route implementation

---
+++ app/microblog/[...slug]/page.tsx
@@ -0,0 +1,81 @@
+import { Microblog } from "@/app/_lib/microblog/definitions";
+import { Metadata } from "next";
+import Breadcrumb from "@/app/_components/breadcrumb";
+import getPost from "@/app/_lib/microblog/getPost";
+import { notFound } from "next/navigation";
+import MicroblogPost from "@/app/_components/microblog/post";
+
+type Props = {
+   params: {
+      slug: string[];
+   };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+   return {
+      title: "post",
+   };
+}
+
+export default async function Post({ params }: { params: { slug: string[] } }) {
+   const slugArray = params.slug;
+   const idFromSlug = params.slug[params.slug.length - 1];
+
+   const isValidId = /^\d{7}$/.test(idFromSlug);
+   if (!isValidId) {
+      notFound();
+   }
+
+   let post: Microblog;
+
+   if (slugArray.length === 1) {
+      try {
+         post = await getPost(idFromSlug);
+      } catch {
+         notFound();
+      }
+   } else if (
+      slugArray.length === 4 &&
+      /^\d{4}$/.test(slugArray[0]) &&
+      /^[1-9]\d?$/.test(slugArray[1]) &&
+      /^[1-9]\d?$/.test(slugArray[2])
+   ) {
+      const year = slugArray[0];
+      const month = slugArray[1];
+      const day = slugArray[2];
+
+      try {
+         post = await getPost(idFromSlug, { year, month, day });
+      } catch {
+         notFound();
+      }
+   } else {
+      notFound();
+   }
+
+   return (
+      <div>
+         <header className="sticky top-0 z-50">
+            <div className="bg-white px-4 pb-1 pt-7 md:pt-[44px] lg:pt-[60px] dark:bg-stone-900">
+               <Breadcrumb
+                  items={[
+                     { type: "link", text: "Jade", href: "/" },
+                     { type: "separator" },
+                     {
+                        type: "link",
+                        text: "Microblog",
+                        href: "/microblog",
+                     },
+                     { type: "separator" },
+                     { type: "text", text: "This post" },
+                  ]}
+               />
+            </div>
+            <div className="bg-gradient-to-b from-white px-4 pb-4 dark:from-stone-900"></div>
+         </header>
+         <main className="px-4">
+            <MicroblogPost post={post} dynamic_time={true} />
+         </main>
+      </div>
+   );
+}
