e44eaceffbc398ee4f0dda9a566dfc40a1ecdc8e
Jade van Dorsten <jade@inthetrees.me>
2024-05-14 19:51:19 -0500

    Fetch and conditionally add Micro.blog external link

--- app/microblog/[...slug]/page.tsx
+++ app/microblog/[...slug]/page.tsx
@@ -1,9 +1,10 @@
-import { Microblog } from "@/app/_lib/microblog/definitions";
+import { Microblog, Microdotblog } from "@/app/_lib/microblog/definitions";
 import { Metadata } from "next";
 import Breadcrumb from "@/app/_components/breadcrumb";
 import getPost from "@/app/_lib/microblog/getPost";
 import { notFound } from "next/navigation";
 import MicroblogPost from "@/app/_components/microblog/post";
+import getMicrodotblog from "@/app/_lib/microblog/getMicrodotblog";
 
 type Props = {
    params: {
@@ -65,23 +66,51 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function Post({ params }: { params: { slug: string[] } }) {
    const post = await getMicroblog({ params });
+
+   let microdotblog: Microdotblog | null = null;
+   try {
+      microdotblog = await getMicrodotblog(post.url);
+   } catch {
+      console.error("Failed to fetch conversation from Micro.blog");
+   }
+
    return (
       <div>
          <header className="sticky top-0 z-50">
             <div className="bg-white px-4 pb-1 pt-7 md:pt-[44px] lg:pt-[60px] dark:bg-stone-900">
-               <Breadcrumb
-                  items={[
-                     { type: "link", text: "Jade", href: "/" },
-                     { type: "separator" },
-                     {
-                        type: "link",
-                        text: "Microblog",
-                        href: "/microblog",
-                     },
-                     { type: "separator" },
-                     { type: "text", text: "This post" },
-                  ]}
-               />
+               {microdotblog && microdotblog.home_page_url ?
+                  <Breadcrumb
+                     items={[
+                        { type: "link", text: "Jade", href: "/" },
+                        { type: "separator" },
+                        {
+                           type: "link",
+                           text: "Microblog",
+                           href: "/microblog",
+                        },
+                        { type: "separator" },
+                        { type: "text", text: "This post" },
+                        {
+                           type: "external-link",
+                           text: "Micro.blog",
+                           href: microdotblog.home_page_url,
+                        },
+                     ]}
+                  />
+               :  <Breadcrumb
+                     items={[
+                        { type: "link", text: "Jade", href: "/" },
+                        { type: "separator" },
+                        {
+                           type: "link",
+                           text: "Microblog",
+                           href: "/microblog",
+                        },
+                        { type: "separator" },
+                        { type: "text", text: "This post" },
+                     ]}
+                  />
+               }
             </div>
             <div className="bg-gradient-to-b from-white px-4 pb-4 dark:from-stone-900"></div>
          </header>
