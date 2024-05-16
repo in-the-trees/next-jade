06fa62b499b4d453b15f6b47f5809f981807d6f1
Jade van Dorsten <jade@inthetrees.me>
2024-05-15 22:56:38 -0500

    Clean up/optimise timestamp formatter

--- app/_lib/relativeTime.ts
+++ app/_lib/relativeTime.ts
@@ -1,48 +1,40 @@
+const non_rel_format: Intl.DateTimeFormatOptions = {
+   month: "short",
+   day: "2-digit",
+   hour: "2-digit",
+   minute: "2-digit",
+   hour12: false,
+};
+const rel_format: Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat("en", {
+   numeric: "always",
+});
+const rel_units: { unit: Intl.RelativeTimeFormatUnit; seconds: number }[] = [
+   { unit: "week", seconds: 604800 },
+   { unit: "day", seconds: 86400 },
+   { unit: "hour", seconds: 3600 },
+   { unit: "minute", seconds: 60 },
+   { unit: "second", seconds: 1 },
+];
+
 export const formatTimeRelatively = (
    date: Date | string,
    dynamic?: boolean,
 ): string => {
-   const now = new Date();
+   const now: Date = new Date();
 
    if (typeof date === "string") {
       date = new Date(date);
    }
 
-   const rtf = new Intl.RelativeTimeFormat("en", { numeric: "always" });
    const diff = (date.getTime() - now.getTime()) / 1000;
 
-   const oneWeeksInSeconds = 1 * 7 * 24 * 60 * 60;
-   if (!dynamic || Math.abs(diff) > oneWeeksInSeconds) {
-      const options: Intl.DateTimeFormatOptions = {
-         month: "short",
-         day: "2-digit",
-         hour: "2-digit",
-         minute: "2-digit",
-         hour12: false,
-      };
-      return date.toLocaleString("en-US", options).replace(" at ", ", ");
+   if (!dynamic || Math.abs(diff) > rel_units[0].seconds) {
+      return date.toLocaleString("en-US", non_rel_format).replace(" at ", ", ");
    }
 
-   const units: Intl.RelativeTimeFormatUnit[] = [
-      "year",
-      "quarter",
-      "month",
-      "week",
-      "day",
-      "hour",
-      "minute",
-      "second",
-   ];
-
-   const divisors = [31536000, 7776000, 2592000, 604800, 86400, 3600, 60, 1];
-
-   for (let i = 0; i < units.length; i++) {
-      if (Math.abs(diff) >= divisors[i]) {
-         const relativeDate: string = rtf.format(
-            Math.round(diff / divisors[i]),
-            units[i],
-         );
-         return relativeDate;
+   for (const { unit, seconds } of rel_units) {
+      if (Math.abs(diff) >= seconds) {
+         return rel_format.format(Math.round(diff / seconds), unit);
       }
    }
 
