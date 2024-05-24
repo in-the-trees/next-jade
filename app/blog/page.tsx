f0d13c42ac5ad2bc201b7961326f11070dfe38c9
Jade van Dorsten <jade@inthetrees.me>
2024-05-24 13:28:27 -0500

    Disable blog (for now)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,8 @@ import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import Breadcrumb from "@/app/_components/breadcrumb";
 
 export default async function Blog() {
-   const posts = (await getPosts(PostReturn.MATTER_ONLY)) as PostMatter[];
+   const posts = [] as PostMatter[];
+   //const posts = (await getPosts(PostReturn.MATTER_ONLY)) as PostMatter[];
 
    return (
       <div>
@@ -35,33 +36,37 @@ export default async function Blog() {
             </h2>
 
             <ul className="mt-9">
-               {posts
-                  .sort((a, b) => (a.date > b.date ? -1 : 1))
-                  .map((post) => (
-                     <li key={post.slug} className="mb-3.5">
-                        <div className="flex items-center gap-[0.25em] leading-none">
-                           <time
-                              dateTime={post.date}
-                              className={`${commit_mono.className} text-[1em - 1px] block self-start text-nowrap text-[calc(1em-1px)] text-gray-500 dark:text-stone-400`}
-                           >
-                              {new Date(post.date + "T00:00:00").toLocaleString(
-                                 "default",
-                                 {
+               {posts.length > 0 ?
+                  posts
+                     .sort((a, b) => (a.date > b.date ? -1 : 1))
+                     .map((post) => (
+                        <li key={post.slug} className="mb-3.5">
+                           <div className="flex items-center gap-[0.25em] leading-none">
+                              <time
+                                 dateTime={post.date}
+                                 className={`${commit_mono.className} text-[1em - 1px] block self-start text-nowrap text-[calc(1em-1px)] text-gray-500 dark:text-stone-400`}
+                              >
+                                 {new Date(
+                                    post.date + "T00:00:00",
+                                 ).toLocaleString("default", {
                                     month: "short",
                                     day: "numeric",
-                                 },
-                              )}
-                           </time>
-                           <ChevronRightIcon className="block h-3 w-3 self-start text-nowrap text-gray-400 dark:text-stone-500" />
-                           <Link
-                              href={`/blog/${post.slug}`}
-                              className="block self-start text-blue-500 hover:underline dark:text-violet-400"
-                           >
-                              {post.title}
-                           </Link>
-                        </div>
-                     </li>
-                  ))}
+                                 })}
+                              </time>
+                              <ChevronRightIcon className="block h-3 w-3 self-start text-nowrap text-gray-400 dark:text-stone-500" />
+                              <Link
+                                 href={`/blog/${post.slug}`}
+                                 className="block self-start text-blue-500 hover:underline dark:text-violet-400"
+                              >
+                                 {post.title}
+                              </Link>
+                           </div>
+                        </li>
+                     ))
+               :  <p className="text-gray-400 dark:text-stone-500">
+                     No posts yet.
+                  </p>
+               }
             </ul>
          </main>
       </div>
