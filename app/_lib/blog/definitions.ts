42823bd577dbf4fd1c4a02546aa24ebeda842c92
Jade van Dorsten <jade@inthetrees.me>
2024-06-05 21:08:23 -0500

    Update PostMatter definition
    Support categories and tags

--- app/_lib/blog/definitions.ts
+++ app/_lib/blog/definitions.ts
@@ -1,15 +1,18 @@
+export type PostCategories = "tech" | "life";
+
 export enum PostReturn {
    FULL,
    MATTER_ONLY,
 }
 
 export interface PostMatter {
-   filename?: string;
-   slug: string;
-   date: string;
-   title: string;
-   description?: string;
    draft?: boolean;
+   slug: string;
+   category: PostCategories;
+   title: string;
+   date: string;
+   description?: string;
+   tags?: string[];
 }
 
 export interface Post {
