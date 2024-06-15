334617d9fe4506c69f2593caeac4a2861f748c7e
Jade van Dorsten <jade@inthetrees.me>
2024-06-14 21:01:22 -0500

    Update microblog post page with progressive blur

--- app/microblog/[...slug]/page.tsx
+++ app/microblog/[...slug]/page.tsx
@@ -33,8 +33,8 @@ export default async function Post({ params }: { params: { slug: string[] } }) {
 
    return (
       <div>
-         <header className="sticky top-0 z-30">
-            <div className="bg-white px-4 pb-1 pt-[24px] md:pt-[40px] lg:pt-[56px] dark:bg-stone-900">
+         <header className="z-40">
+            <div className="pb-4 dark:bg-stone-900">
                {microdotblog && microdotblog.home_page_url ?
                   <div className="max-w flex flex-row items-center justify-between gap-2 py-[-.rem]">
                      <Breadcrumb
@@ -70,9 +70,16 @@ export default async function Post({ params }: { params: { slug: string[] } }) {
                   />
                }
             </div>
-            <div className="bg-gradient-to-b from-white px-4 pb-4 dark:from-stone-900"></div>
          </header>
-         <main className="px-4">
+         <div className="gradient-blur">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
+         <main>
             <MicroblogPost post={post} dynamic_time={true} />
             <ReplyArea post={post} microdotblog={microdotblog} />
          </main>
