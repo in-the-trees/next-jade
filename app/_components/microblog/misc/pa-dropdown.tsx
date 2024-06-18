26600e2f3d966ab0cd3f90fdc73cdfdc380f4801
Jade van Dorsten <jade@inthetrees.me>
2024-06-18 18:06:46 -0500

    Update post actions dropdown iconography

--- app/_components/microblog/misc/pa-dropdown.tsx
+++ app/_components/microblog/misc/pa-dropdown.tsx
@@ -3,11 +3,11 @@
 import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import { clsx } from "clsx";
-import { EllipsisHorizontalCircleIcon } from "@heroicons/react/24/outline";
 import {
-   LinkIcon,
-   ArrowRightStartOnRectangleIcon,
-} from "@heroicons/react/20/solid";
+   EllipsisCircleMedium20Icon,
+   ArrowUpRightCircleMedium16Icon,
+   LinkMedium16Icon,
+} from "@/app/_components/icons";
 
 type PADropdownProps = {
    postUrl: string;
@@ -39,9 +39,9 @@ const PADropdown = ({ postUrl, homePageUrl }: PADropdownProps) => {
       <div ref={dropdownRef} className="relative">
          <button
             onClick={() => setIsOpen(!isOpen)}
-            className="rounded-full p-1 transition-transform ease-out hover:bg-stone-200 active:scale-[0.95] dark:hover:bg-stone-600/30"
+            className="rounded-full transition-transform ease-out hover:opacity-70 active:scale-[0.95]"
          >
-            <EllipsisHorizontalCircleIcon className="h-5 w-5 text-stone-500 dark:text-stone-300/70" />
+            <EllipsisCircleMedium20Icon className="h-5 w-5 text-stone-500 dark:text-stone-300/70" />
          </button>
          {isOpen && (
             <div className="absolute right-0 z-10 mt-2 w-[12rem] rounded-2xl border border-stone-200 bg-stone-100 text-forest-950 shadow-xl shadow-stone-400/10 dark:border-stone-400/40 dark:bg-stone-500 dark:text-stone-50 dark:shadow-stone-700/10">
@@ -59,7 +59,7 @@ const PADropdown = ({ postUrl, homePageUrl }: PADropdownProps) => {
                         className="mb-1 flex w-full items-center justify-between rounded-xl bg-forest-100 px-4 py-2 text-left text-sm font-normal-mid text-forest-700 transition-transform ease-out hover:scale-[1.01] hover:bg-forest-200 dark:bg-forest-200 dark:text-forest-800 dark:hover:bg-forest-300"
                      >
                         Micro.blog
-                        <ArrowRightStartOnRectangleIcon className="h-4 w-4 fill-forest-700 stroke-2 dark:fill-forest-800" />
+                        <ArrowUpRightCircleMedium16Icon className="h-4 w-4 fill-forest-700 stroke-2 dark:fill-forest-800" />
                      </Link>
                   )}
                   <button
@@ -71,7 +71,7 @@ const PADropdown = ({ postUrl, homePageUrl }: PADropdownProps) => {
                      }}
                   >
                      Copy link
-                     <LinkIcon className="h-4 w-4 fill-forest-950 stroke-2 dark:fill-stone-50" />
+                     <LinkMedium16Icon className="h-4 w-4 fill-forest-950 stroke-2 dark:fill-stone-50" />
                   </button>
                </div>
                {homePageUrl && id && (
