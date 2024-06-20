0d38e776c51c56ee9943cf7cea300608bf691422
Jade van Dorsten <jade@inthetrees.me>
2024-06-20 18:46:41 -0500

    Nuke microblog
    And thus begins the transition to Bluesky

--- app/_lib/microblog/getPosts.ts
+++
@@ -1,9 +0,0 @@
-export default async function getPosts(feed: string) {
-   const res = await fetch(feed);
-   if (!res.ok) {
-      throw new Error("Failed to fetch feed");
-   } else {
-      const json = await res.json();
-      return json.items;
-   }
-}
