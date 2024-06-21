b6f30d480c06e94fb7566e81e4ce02818cac36eb
Jade van Dorsten <jade@inthetrees.me>
2024-06-20 21:26:24 -0500

    Destruct API responses

--- app/microblog/page.tsx
+++ app/microblog/page.tsx
@@ -5,11 +5,13 @@ import { lora } from "@/app/_fonts/fonts";
 const ACTOR = `${process.env.NEXT_PUBLIC_BSKY_HANDLE}`;
 
 export default async function BlueskyPage() {
-   const profile = await agent.app.bsky.actor.getProfile({
+   const { data: profile } = await agent.app.bsky.actor.getProfile({
       actor: ACTOR,
    });
 
-   const posts = await agent.app.bsky.feed.getAuthorFeed({
+   const {
+      data: { feed: posts },
+   } = await agent.app.bsky.feed.getAuthorFeed({
       actor: ACTOR,
       filter: "posts_and_author_threads",
    });
@@ -45,10 +47,14 @@ export default async function BlueskyPage() {
 
             <div className="my-4 flex flex-wrap items-center gap-2">
                <div className="mt-9">
-                  <h3 className="text-2xl font-medium-mid">Profile</h3>
-                  <pre>{JSON.stringify(profile.data, null, 2)}</pre>
-                  <h3 className="text-2xl font-medium-mid">Posts</h3>
-                  <pre>{JSON.stringify(posts.data, null, 2)}</pre>
+                  <h3 className="my-4 text-2xl font-medium-mid">Profile</h3>
+                  <pre>{JSON.stringify(profile, null, 2)}</pre>
+                  <h3 className="my-4 text-2xl font-medium-mid">Posts</h3>
+                  {posts.map(({ post }) => (
+                     <div key={post.cid} className="my-4">
+                        <pre>{JSON.stringify(post, null, 2)}</pre>
+                     </div>
+                  ))}
                </div>
             </div>
          </main>
