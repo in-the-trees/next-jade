928bc9b0a3ff7cc9cf74b64729d92a8cb05c7822
Jade van Dorsten <jade@inthetrees.me>
2024-06-27 17:18:07 -0500

    Create bskyImg funciton
    To generate a URL with customized options, such as size and format

--- app/_lib/microblog/bskyImg.ts
+++ app/_lib/microblog/bskyImg.ts
@@ -1,3 +1,26 @@
 export function getLinkFromUrl(url: string): string {
    return url.match(/\/([^\/]*)@./)?.[1] ?? "";
 }
+
+enum Format {
+   JPEG = "jpeg",
+   AVIF = "avif",
+   WEBP = "webp",
+}
+
+enum Size {
+   THUMB = "thumb",
+   FULL = "full",
+}
+
+export function bskyImg(
+   did: string,
+   ref: string,
+   format: Format,
+   size: Size = Size.FULL,
+): string {
+   return `https://cdn.bsky.app/img/feed_${size === Size.FULL ? "fullsize" : "thumbnail"}/plain/${did}/${ref}@${format}`;
+}
+
+bskyImg.Format = Format;
+bskyImg.Size = Size;
