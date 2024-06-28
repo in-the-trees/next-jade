c97acbb2d6cc0b7b082bb8690963e8c17094aef0
Jade van Dorsten <jade@inthetrees.me>
2024-06-27 21:58:12 -0500

    Refine temporary image styling

--- app/_components/microblog/post.tsx
+++ app/_components/microblog/post.tsx
@@ -14,49 +14,50 @@ function PostImages({ post }: { post: PostType }) {
    return (
       <div>
          {post.embed.images.map((i) => (
-            <a
-               href={bskyImg(
-                  post.author.did,
-                  getLinkFromUrl(i.thumb),
-                  bskyImg.Format.AVIF,
-                  bskyImg.Size.FULL,
-               )}
-               target="_blank"
-               key={i.thumb}
-            >
-               <picture>
-                  <source
-                     srcSet={bskyImg(
-                        post.author.did,
-                        getLinkFromUrl(i.thumb),
-                        bskyImg.Format.AVIF,
-                        bskyImg.Size.THUMB,
-                     )}
-                     type="image/avif"
-                  />
-                  <source
-                     srcSet={bskyImg(
-                        post.author.did,
-                        getLinkFromUrl(i.thumb),
-                        bskyImg.Format.WEBP,
-                        bskyImg.Size.THUMB,
-                     )}
-                     type="image/webp"
-                  />
-                  <img
-                     src={bskyImg(
-                        post.author.did,
-                        getLinkFromUrl(i.thumb),
-                        bskyImg.Format.JPEG,
-                        bskyImg.Size.THUMB,
-                     )}
-                     alt={i.alt}
-                     height={i.aspectRatio ? i.aspectRatio.height : undefined}
-                     width={i.aspectRatio ? i.aspectRatio.width : undefined}
-                     className="my-3.5 max-w-96 rounded-xl bg-stone-200 transition-transform ease-out hover:scale-[1.01] dark:bg-stone-600"
-                  />
-               </picture>
-            </a>
+            <div key={i.thumb}>
+               <a
+                  href={bskyImg(
+                     post.author.did,
+                     getLinkFromUrl(i.thumb),
+                     bskyImg.Format.AVIF,
+                     bskyImg.Size.FULL,
+                  )}
+                  target="_blank"
+               >
+                  <picture>
+                     <source
+                        srcSet={bskyImg(
+                           post.author.did,
+                           getLinkFromUrl(i.thumb),
+                           bskyImg.Format.AVIF,
+                           bskyImg.Size.THUMB,
+                        )}
+                        type="image/avif"
+                     />
+                     <source
+                        srcSet={bskyImg(
+                           post.author.did,
+                           getLinkFromUrl(i.thumb),
+                           bskyImg.Format.WEBP,
+                           bskyImg.Size.THUMB,
+                        )}
+                        type="image/webp"
+                     />
+                     <img
+                        src={bskyImg(
+                           post.author.did,
+                           getLinkFromUrl(i.thumb),
+                           bskyImg.Format.JPEG,
+                           bskyImg.Size.THUMB,
+                        )}
+                        alt={i.alt}
+                        height={i.aspectRatio ? i.aspectRatio.height : undefined}
+                        width={i.aspectRatio ? i.aspectRatio.width : undefined}
+                        className="my-3.5 inline-block max-w-72 rounded-xl bg-stone-200 transition-transform ease-out hover:scale-[1.01] dark:bg-stone-600"
+                     />
+                  </picture>
+               </a>
+            </div>
          ))}
       </div>
    );
