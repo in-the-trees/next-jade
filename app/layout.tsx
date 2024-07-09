c13a5e5ec47a6106c4d032e5e11a0b0104302584
Jade van Dorsten <jade@inthetrees.me>
2024-07-09 16:47:22 -0500

    Update meta dark color scheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
    themeColor: [
       { media: "(prefers-color-scheme: light)", color: "#f5f5f4" },
-      { media: "(prefers-color-scheme: dark)", color: "#44403c" },
+      { media: "(prefers-color-scheme: dark)", color: "#000000" },
    ],
 };
 
