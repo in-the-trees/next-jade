735d01dbc27063a9845e2a5507e0a4d15a8b3fd6
Jade van Dorsten <jade@inthetrees.me>
2024-07-16 23:46:24 -0500

    Update width of index page

--- app/(router)/page.tsx
+++ app/(router)/page.tsx
@@ -6,11 +6,11 @@ export async function generateStaticParams() {
    return [{ slug: "colophon" }, { slug: "web-presence" }];
 }
 
-export default function Home() {
+export default function Index() {
    return (
       <main id="home">
          <div className="flex w-full pt-9 md:justify-center md:pt-[44px] lg:pt-[60px]">
-            <Jade className="w-full max-w-xl px-4" home />
+            <Jade className="w-full max-w-[27rem] px-4" home />
          </div>
       </main>
    );
